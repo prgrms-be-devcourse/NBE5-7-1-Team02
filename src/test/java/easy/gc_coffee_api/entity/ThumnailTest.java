199382f8a3f1ff1048afe3a140dbc7d7056bd244
hunko <rhwlgns4386@naver.com>
2025-04-23 17:27:56 +0900

    test(menu) : 썸네일 테스트 작성

---
+++ src/test/java/easy/gc_coffee_api/entity/ThumnailTest.java
@@ -0,0 +1,19 @@
+package easy.gc_coffee_api.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ThumnailTest {
+
+    @Test
+    void 이미지_타입아닐시_예외(){
+        assertThatThrownBy(() -> new Thumnail(1L,"text/html")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이미지_정상_생성(){
+        assertDoesNotThrow(() -> new Thumnail(1L,"image/png"));
+    }
+}
