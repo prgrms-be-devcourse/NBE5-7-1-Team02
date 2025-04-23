7376bc0bedc579464c3d5327f088de75e44a5b2b
hunko <rhwlgns4386@naver.com>
2025-04-23 11:08:07 +0900

    feat(domain) : 공통엔티티 구현
    앞서 추가하지 못한 엔티티 추가

---
+++ src/main/java/easy/gc_coffee_api/entity/User.java
@@ -0,0 +1,25 @@
+package easy.gc_coffee_api.entity;
+
+import easy.gc_coffee_api.entity.common.BaseDateEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseDateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @Builder
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
