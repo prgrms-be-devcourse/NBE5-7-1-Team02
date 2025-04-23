7bb44ca53da8ef6ee3554f8fff092550c37e9c8e
Soohyeok Kim <devshk447@gmail.com>
2025-04-23 15:39:39 +0900

    feat(repository): 리포지터리 생성

--- src/main/java/easy/gc_coffee_api/repository/OrderRepository.java
+++ src/main/java/easy/gc_coffee_api/repository/OrderRepository.java
@@ -1,4 +1,7 @@
 package easy.gc_coffee_api.repository;
 
-public class OrderRepository {
+import easy.gc_coffee_api.entity.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Orders, Long> {
 }
