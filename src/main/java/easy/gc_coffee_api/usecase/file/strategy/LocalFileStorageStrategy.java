e7daabac66f83f7a13aa0bb9f0894e00cab47dd6
Soohyeok Kim <devshk447@gmail.com>
2025-04-24 11:56:26 +0900

    fix(storage): 서버 외부 로컬 디렉토리를 사용하도록 설정

--- src/main/java/easy/gc_coffee_api/usecase/file/strategy/LocalFileStorageStrategy.java
+++ src/main/java/easy/gc_coffee_api/usecase/file/strategy/LocalFileStorageStrategy.java
@@ -2,6 +2,7 @@ package easy.gc_coffee_api.usecase.file.strategy;
 
 import easy.gc_coffee_api.usecase.file.vo.FileUrl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,17 +14,15 @@ import java.nio.file.Paths;
 @RequiredArgsConstructor
 @Component("localStorage")
 public class LocalFileStorageStrategy implements StorageStrategy {
-    private static final String BASE_PATH = System.getProperty("user.dir");
-    private static final String DIR = "/storage/";
+    @Value("${file.storage_path}")
+    private String STORAGE_PATH;
 
     @Override
     public FileUrl upload(MultipartFile file, String newFileName) throws IOException {
-        Path savePath = Paths.get(BASE_PATH + DIR + newFileName);
-
-        Files.createDirectories(savePath.getParent());
+        Path savePath = Paths.get(STORAGE_PATH + newFileName);
 
         file.transferTo(savePath.toFile());
 
-        return new FileUrl(DIR + newFileName);
+        return new FileUrl(newFileName);
     }
 }
