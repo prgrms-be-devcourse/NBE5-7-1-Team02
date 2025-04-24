78ba8b53cc8dc3b50c7b86515637d10128603d96
Taeho KIM <laferrari963@naver.com>
2025-04-24 17:03:14 +0900

    fix(UpdateMenuRequestDto): price 타입 변경
    int형은 애초에 null을 받을 수 없음. Integer로 변경

--- src/main/java/easy/gc_coffee_api/dto/UpdateMenuRequestDto.java
+++ src/main/java/easy/gc_coffee_api/dto/UpdateMenuRequestDto.java
@@ -14,7 +14,7 @@ public class UpdateMenuRequestDto {
 
     @NotNull(message="가격을 입력해주세요.")
     @Min(value=0, message="가격은 음수일 수 없습니다.")
-    private int price;
+    private Integer price;
 
     @NotBlank(message="카테고리를 입력해주세요.")
     private String category;
