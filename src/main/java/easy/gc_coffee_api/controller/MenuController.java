9985aac43d6be230b24903a7c91d64a1665d7a85
Taeho KIM <laferrari963@naver.com>
2025-04-24 09:59:24 +0900

    feat(MenuController): 업데이트 메뉴 컨트롤러 작성

--- src/main/java/easy/gc_coffee_api/controller/MenuController.java
+++ src/main/java/easy/gc_coffee_api/controller/MenuController.java
@@ -1,5 +1,9 @@
 package easy.gc_coffee_api.controller;
 
+import easy.gc_coffee_api.dto.UpdateMenuRequestDto;
+import easy.gc_coffee_api.usecase.menu.UpdateMenuUsecase;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import easy.gc_coffee_api.dto.CreateMenuRequestDto;
 import easy.gc_coffee_api.dto.CreateMenuResponseDto;
 import easy.gc_coffee_api.exception.GCException;
@@ -7,7 +11,6 @@ import easy.gc_coffee_api.exception.ThumnailCreateException;
 import easy.gc_coffee_api.usecase.menu.CreateMenuUsecase;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MenuController {
 
     private final CreateMenuUsecase createMenuUsecase;
+    private final UpdateMenuUsecase updateMenuUsecase;
 
     @PostMapping("/menus")
     public ResponseEntity<CreateMenuResponseDto> create(@RequestBody @Validated CreateMenuRequestDto requestDto) {
@@ -32,5 +36,11 @@ public class MenuController {
         } catch (ThumnailCreateException | EntityNotFoundException e ) {
             throw new GCException(e.getMessage(), e, 400);
         }
+    }
+
+    @PutMapping("/menus/{menuId}")
+    public ResponseEntity<String> updateMenu(@PathVariable Long menuId,@RequestBody UpdateMenuRequestDto requestDto ) {
+        updateMenuUsecase.execute(menuId, requestDto);
+        return ResponseEntity.ok("수정 완료");
     }
 }
