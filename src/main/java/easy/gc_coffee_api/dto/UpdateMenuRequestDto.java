b675b00fddd7470032beaec855c1dadaebb5176a
Taeho KIM <laferrari963@naver.com>
2025-04-24 10:18:11 +0900

    refactor(UpdateMenuRequestDto): 생성자 추가

--- src/main/java/easy/gc_coffee_api/dto/UpdateMenuRequestDto.java
+++ src/main/java/easy/gc_coffee_api/dto/UpdateMenuRequestDto.java
@@ -1,8 +1,10 @@
 package easy.gc_coffee_api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UpdateMenuRequestDto {
     private String name;
     private int price;
