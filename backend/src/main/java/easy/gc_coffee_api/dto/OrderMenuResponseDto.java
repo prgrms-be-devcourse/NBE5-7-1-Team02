1d00b8c174e183a582b20c0aa7d9bfdf893b4183
Taeho KIM <laferrari963@naver.com>
2025-04-25 14:36:09 +0900

    feat(OrderListResponseDto,OrderMenuReponseDto): 주문 반환 dto 생성

---
+++ backend/src/main/java/easy/gc_coffee_api/dto/OrderMenuResponseDto.java
@@ -0,0 +1,14 @@
+package easy.gc_coffee_api.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class OrderMenuResponseDto {
+    private String menuName;
+    private Integer price;
+    private Integer quantity;
+    private String thumbnailUrl;
+}
