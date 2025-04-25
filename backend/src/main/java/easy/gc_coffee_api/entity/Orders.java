d94f2cf01b7489405ebc435fef3b68ba8f1b535b
Taeho KIM <laferrari963@naver.com>
2025-04-25 14:36:27 +0900

    feat(Orders): 관계삽입

--- backend/src/main/java/easy/gc_coffee_api/entity/Orders.java
+++ backend/src/main/java/easy/gc_coffee_api/entity/Orders.java
@@ -4,6 +4,9 @@ import easy.gc_coffee_api.entity.common.BaseDateEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -16,6 +19,9 @@ public class Orders extends BaseDateEntity {
     private Address address;
     @Setter
     private Integer totalPrice;
+
+    @OneToMany(mappedBy = "orders", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<OrderMenu> orderMenus = new ArrayList<>();
 
     @Builder
     public Orders(String email, Address address, Integer totalPrice) {
