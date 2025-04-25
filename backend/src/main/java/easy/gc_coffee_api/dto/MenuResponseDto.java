9bc7bfc5604a8e784c825944da02b2b0a48f33b6
hunko <rhwlgns4386@naver.com>
2025-04-25 14:18:49 +0900

    refactor(menu) : MenuResponseDto 필요없는 생성자 및 기능 제거

--- backend/src/main/java/easy/gc_coffee_api/dto/MenuResponseDto.java
+++ backend/src/main/java/easy/gc_coffee_api/dto/MenuResponseDto.java
@@ -1,6 +1,5 @@
 package easy.gc_coffee_api.dto;
 
-import easy.gc_coffee_api.entity.File;
 import easy.gc_coffee_api.entity.common.Category;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -25,26 +24,6 @@ public class MenuResponseDto {
         this.category = category;
         this.fileId = fileId;
         this.thumbnailUrl = thumbnailUrl;
-    }
-
-    public MenuResponseDto(Long id, String name, int price, Category category, File file){
-        this(id, name, price, category, getUrl(file), getFileId(file));
-    }
-
-    private static String getUrl(File file) {
-        if (file == null) {
-            return null;
-        }
-        // 임의로 지정하였습니다. -> 추후 수정 필요합니다.
-        String prefix = "http://localhost:8084/";
-        return prefix + file.getKey();
-    }
-
-    private static Long getFileId(File file) {
-        if (file == null) {
-            return null;
-        }
-        return file.getId();
     }
 
 }
