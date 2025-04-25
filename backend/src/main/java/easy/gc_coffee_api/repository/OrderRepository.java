17072ebd35971f475185dc5aca353c5ed2c66b0b
Taeho KIM <laferrari963@naver.com>
2025-04-25 14:36:46 +0900

    feat(OrderRepository): 주문목록 반환 쿼리 임시저장

--- backend/src/main/java/easy/gc_coffee_api/repository/OrderRepository.java
+++ backend/src/main/java/easy/gc_coffee_api/repository/OrderRepository.java
@@ -2,6 +2,14 @@ package easy.gc_coffee_api.repository;
 
 import easy.gc_coffee_api.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
+
+    @Query("SELECT o From Orders o"+
+            " JOIN FETCH o.orderMenus om"+
+            " JOIN FETCH om.menu m")
+    List<Orders> findAllWithMenus();
 }
