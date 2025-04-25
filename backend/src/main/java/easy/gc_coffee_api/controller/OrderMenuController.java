82395548caf92355389b5384e09e6a6fc571ca85
Taeho KIM <laferrari963@naver.com>
2025-04-25 14:35:36 +0900

    feat(OrderMenuController): GET메서드 작성

--- backend/src/main/java/easy/gc_coffee_api/controller/OrderMenuController.java
+++ backend/src/main/java/easy/gc_coffee_api/controller/OrderMenuController.java
@@ -1,14 +1,18 @@
 package easy.gc_coffee_api.controller;
 
 import easy.gc_coffee_api.dto.OrderRequestDto;
+import easy.gc_coffee_api.dto.OrderListResponseDto;
 import easy.gc_coffee_api.usecase.order.OrderMenuUserCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +23,10 @@ public class OrderMenuController {
   @PostMapping("/orders")
   public ResponseEntity<Long> post(@Valid @RequestBody OrderRequestDto order) {
     return new ResponseEntity<>(orderMenuUserCase.execute(order), HttpStatus.CREATED);
+  }
+
+  @GetMapping("/orders")
+  public ResponseEntity<List<OrderListResponseDto>> getAllOrders() {
+    return ResponseEntity.ok(adminOrderListUsecase.execute());
   }
 }
