d5d0758c42f20f504bae6060d543e96bc6fb6424
hunko <rhwlgns4386@naver.com>
2025-04-26 17:19:37 +0900

    chore(order) : 비밀번호 인코더 추가

--- backend/src/main/java/easy/gc_coffee_api/config/SecurityConfig.java
+++ backend/src/main/java/easy/gc_coffee_api/config/SecurityConfig.java
@@ -8,7 +8,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -59,5 +60,10 @@ public class SecurityConfig {
                         .requestMatchers("/admin/login", "/orders").permitAll()
                         .anyRequest().authenticated()
                 ).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
