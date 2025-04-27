951835af2e6597638776ebbb4b84f9948dd0f542
Taeho KIM <laferrari963@naver.com>
2025-04-27 16:28:14 +0900

    refactor(ShipOrdersUseCase): 예외처리 수정

--- backend/src/main/java/easy/gc_coffee_api/usecase/order/ShipOrdersUseCase.java
+++ backend/src/main/java/easy/gc_coffee_api/usecase/order/ShipOrdersUseCase.java
@@ -1,6 +1,8 @@
 package easy.gc_coffee_api.usecase.order;
 
 
+import easy.gc_coffee_api.exception.order.OrderNotFoundException;
+import easy.gc_coffee_api.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -15,8 +17,12 @@ import java.util.List;
 public class ShipOrdersUseCase {
 
     private final JdbcTemplate jdbcTemplate;
+    private final OrderRepository orderRepository;
 
     public void execute(List<Long> ids) {
+        ids.forEach(id -> {
+            orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException("주문이 존재하지 않습니다. 주문 ID:" +id,404 ));
+        });
         jdbcTemplate.batchUpdate(
                 "UPDATE orders SET status = 'SHIPPED' WHERE id = ?", new BatchPreparedStatementSetter() {
                     @Override
