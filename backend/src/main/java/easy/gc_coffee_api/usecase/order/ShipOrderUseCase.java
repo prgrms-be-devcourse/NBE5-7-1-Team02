fe85138e5899803a397215f7ddf45d2329ed05c5
Taeho KIM <laferrari963@naver.com>
2025-04-27 14:06:18 +0900

    feat(ShipOrderUseCase): 단일주문 구현

---
+++ backend/src/main/java/easy/gc_coffee_api/usecase/order/ShipOrderUseCase.java
@@ -0,0 +1,24 @@
+package easy.gc_coffee_api.usecase.order;
+
+import easy.gc_coffee_api.entity.OrderStatus;
+import easy.gc_coffee_api.entity.Orders;
+import easy.gc_coffee_api.exception.menu.MenuNotFoundException;
+import easy.gc_coffee_api.repository.OrderRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ShipOrderUseCase {
+
+    private final OrderRepository orderRepository;
+
+    @Transactional
+    public void execute(Long orderId){
+        Orders order = orderRepository.findById(orderId).orElseThrow(()->new MenuNotFoundException("주문 존재x",400));
+
+        order.ship();
+    }
+}
