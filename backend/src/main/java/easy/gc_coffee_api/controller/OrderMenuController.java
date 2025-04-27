939c2ab0f98731e35bec6050f9b788ea5bf359c1
Taeho KIM <laferrari963@naver.com>
2025-04-27 14:04:45 +0900

    feat(OrderMenuController): 전체주문, 단일주문 컨트롤러 작성

--- backend/src/main/java/easy/gc_coffee_api/controller/OrderMenuController.java
+++ backend/src/main/java/easy/gc_coffee_api/controller/OrderMenuController.java
@@ -2,18 +2,21 @@ package easy.gc_coffee_api.controller;
 
 import easy.gc_coffee_api.dto.OrderRequestDto;
 import easy.gc_coffee_api.dto.OrderListResponseDto;
+import easy.gc_coffee_api.exception.GCException;
+import easy.gc_coffee_api.exception.menu.MenuNotFoundException;
 import easy.gc_coffee_api.usecase.order.GetOrderListUsecase;
 import easy.gc_coffee_api.usecase.order.OrderMenuUserCase;
+import easy.gc_coffee_api.usecase.order.ShipOrderUseCase;
+import easy.gc_coffee_api.usecase.order.ShipOrdersUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +24,8 @@ public class OrderMenuController {
 
   private final OrderMenuUserCase orderMenuUserCase;
   private final GetOrderListUsecase getOrderListUsecase;
+  private final ShipOrdersUseCase shipOrdersUseCase;
+  private final ShipOrderUseCase shipOrderUseCase;
 
   @PostMapping("/orders")
   public ResponseEntity<Long> post(@Valid @RequestBody OrderRequestDto order) {
@@ -31,4 +36,22 @@ public class OrderMenuController {
   public ResponseEntity<List<OrderListResponseDto>> getAllOrders() {
     return ResponseEntity.ok(getOrderListUsecase.execute());
   }
+
+  @PatchMapping("/orders/ship")
+  public ResponseEntity<Map<String, Boolean>> ship(@RequestBody List<Long> ids) {
+    shipOrdersUseCase.execute(ids);
+    Map<String, Boolean> response = new HashMap<>();
+    response.put("result", true);
+    return ResponseEntity.ok(response);
+  }
+
+
+  @PatchMapping("/orders/{orderId}/ship")
+  public ResponseEntity<Map<String, Boolean>> ship(@PathVariable Long orderId) {
+    shipOrderUseCase.execute(orderId);
+    Map<String, Boolean> response = new HashMap<>();
+    response.put("result", true);
+    return ResponseEntity.ok(response);
+  }
+
 }
